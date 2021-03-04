126521d02ac3e2c040c0ea74b85cf1f7335ce610
Elgingraham <elgin.graham@ocsbstudent.ca>
2021-03-04 14:49:36 +0000

    hhh

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ let upperCaseLetters = /[A-Z]/g
 let numbers = /[0-9]/g
 // checking all the possibilities
 function psw () {
-  if (input.value.match(numbers) && input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){ 
+  if (input.value.match(numbers) && input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)) {
     document.getElementById('result').innerHTML = 'Strong password!'
-  } else if (input.value.match(numbers) && input.value.match(upperCaseLetters)){ 
+  } else if (input.value.match(numbers) && input.value.match(upperCaseLetters)) {
     document.getElementById('result').innerHTML = 'Good password'
-  } else if (input.value.match(numbers) && input.value.match(lowerCaseLetters)){ 
+  } else if (input.value.match(numbers) && input.value.match(lowerCaseLetters)) {
     document.getElementById('result').innerHTML = 'Good password'
-  } else if (input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){ 
+  } else if (input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)) {
     document.getElementById('result').innerHTML = 'Good password'
-  } else if (input.value.match(numbers)){ 
+  } else if (input.value.match(numbers)) {
     document.getElementById('result').innerHTML = 'Bad password'
-  } else if (input.value.match(lowerCaseLetters)){ 
+  } else if (input.value.match(lowerCaseLetters)) {
     document.getElementById('result').innerHTML = 'Bad password'
-  } else if (input.value.match(upperCaseLetters)){ 
+  } else if (input.value.match(upperCaseLetters)) {
     document.getElementById('result').innerHTML = 'Bad password'
   } else { document.getElementById('result').innerHTML = 'Please fill in the box'
   }
