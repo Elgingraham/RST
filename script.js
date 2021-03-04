d51ee0a99a5c74c0bef07f5649f715cf5e7fec13
Elgingraham <elgin.graham@ocsbstudent.ca>
2021-03-04 14:44:50 +0000

    doog

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 document.getElementById('run').addEventListener('click', psw)
-//setting all the varibles
-let input = document.getElementById("password");
-let lowerCaseLetters = /[a-z]/g;
-let upperCaseLetters = /[A-Z]/g;
-let numbers = /[0-9]/g;
-//checking all the possibilities 
-function psw () { 
-  if (input.value.match(numbers) && input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){
- document.getElementById('result').innerHTML = 'Strong password!'
-  } else if (input.value.match(numbers) && input.value.match(upperCaseLetters)){
- document.getElementById('result').innerHTML = 'Good password'
-  } else if (input.value.match(numbers) && input.value.match(lowerCaseLetters)){
- document.getElementById('result').innerHTML = 'Good password'
-  } else if (input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){
- document.getElementById('result').innerHTML = 'Good password'
- } else if (input.value.match(numbers)){
- document.getElementById('result').innerHTML = 'Bad password'
- } else if (input.value.match(lowerCaseLetters)){
- document.getElementById('result').innerHTML = 'Bad password'
- } else if (input.value.match(upperCaseLetters)){
- document.getElementById('result').innerHTML = 'Bad password'
-} else {document.getElementById('result').innerHTML = 'Please fill in the box'
-}
+// setting all the varibles
+let input = document.getElementById('password')
+let lowerCaseLetters = /[a-z]/g
+let upperCaseLetters = /[A-Z]/g
+let numbers = /[0-9]/g
+// checking all the possibilities
+function psw () {
+  if (input.value.match(numbers) && input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){ 
+    document.getElementById('result').innerHTML = 'Strong password!'
+  } else if (input.value.match(numbers) && input.value.match(upperCaseLetters)){ 
+    document.getElementById('result').innerHTML = 'Good password'
+  } else if (input.value.match(numbers) && input.value.match(lowerCaseLetters)){ 
+    document.getElementById('result').innerHTML = 'Good password'
+  } else if (input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){ 
+    document.getElementById('result').innerHTML = 'Good password'
+  } else if (input.value.match(numbers)){ 
+    document.getElementById('result').innerHTML = 'Bad password'
+  } else if (input.value.match(lowerCaseLetters)){ 
+    document.getElementById('result').innerHTML = 'Bad password'
+  } else if (input.value.match(upperCaseLetters)){ 
+    document.getElementById('result').innerHTML = 'Bad password'
+  } else { document.getElementById('result').innerHTML = 'Please fill in the box'
+  }
 }
