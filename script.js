cb7695f821d46c43fd035fc8e73b0335e0fe0de4
Elgingraham <elgin.graham@ocsbstudent.ca>
2021-03-04 14:30:37 +0000

    update

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.getElementById('run').addEventListener('click', psw)
+//setting all the varibles
 let input = document.getElementById("password");
 let lowerCaseLetters = /[a-z]/g;
 let upperCaseLetters = /[A-Z]/g;
 let numbers = /[0-9]/g;
-
+//checking all the possibilities 
 function psw () { 
-  if (input.value.match(numbers && upperCaseLetters && lowerCaseLetters)){
+  if (input.value.match(numbers) && input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){
  document.getElementById('result').innerHTML = 'Strong password!'
-  } else if (input.value.match(numbers && upperCaseLetters )){
+  } else if (input.value.match(numbers) && input.value.match(upperCaseLetters)){
  document.getElementById('result').innerHTML = 'Good password'
-  } else if (input.value.match(numbers && lowerCaseLetters )){
+  } else if (input.value.match(numbers) && input.value.match(lowerCaseLetters)){
  document.getElementById('result').innerHTML = 'Good password'
-  } else if (input.value.match(lowerCaseLetters && upperCaseLetters )){
+  } else if (input.value.match(upperCaseLetters) && input.value.match(lowerCaseLetters)){
  document.getElementById('result').innerHTML = 'Good password'
  } else if (input.value.match(numbers)){
  document.getElementById('result').innerHTML = 'Bad password'
